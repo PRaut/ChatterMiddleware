85aa4126a119eb9053c425bbf2299034d5d90231
Prathamesh Raut <prathamesh.raut001@gmail.com>
2018-04-21 17:06:09 +0530

    CustomRegistration  & Filter Added for chat

--- src/main/java/com/chatterMW/config/WebAppInitializer.java
+++ src/main/java/com/chatterMW/config/WebAppInitializer.java
@@ -1,11 +1,24 @@
 package com.chatterMW.config;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.chatter.config.DBConfig;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		System.out.println("customizeRegistration");
+		registration.setInitParameter("dispatchOptionsRequest", "true");
+		registration.setAsyncSupported(true);
+	}
+	
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		// TODO Auto-generated method stub
@@ -23,5 +36,13 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 		// TODO Auto-generated method stub
 		return new String[] { "/" };
 	}
-
+	
+	@Override
+    protected Filter[] getServletFilters()
+    {
+        CharacterEncodingFilter encodingFilter=new CharacterEncodingFilter();
+        encodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+        return new Filter[] {encodingFilter};
+    }
+	
 }
