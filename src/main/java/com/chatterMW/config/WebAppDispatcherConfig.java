a75356509c0a9d8e172a297a92abc9afead2b911
Prathamesh Raut <prathamesh.raut001@gmail.com>
2018-04-21 17:06:56 +0530

    Multipart Added for Image upload

--- src/main/java/com/chatterMW/config/WebAppDispatcherConfig.java
+++ src/main/java/com/chatterMW/config/WebAppDispatcherConfig.java
@@ -3,24 +3,38 @@ package com.chatterMW.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.chatterMW")
+@ComponentScan(basePackages = "com.chatterMW")
 public class WebAppDispatcherConfig {
 
 	@Bean
-	public ViewResolver viewResolver(){
+	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views");
 		viewResolver.setSuffix(".jsp");
-		
+
 		return viewResolver;
 	}
-	
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
+	}
+
+	// For Image upload
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getMultipartResolver() {
+
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		commonsMultipartResolver.setMaxUploadSize(100000000);
+		return commonsMultipartResolver;
+	}
 }
